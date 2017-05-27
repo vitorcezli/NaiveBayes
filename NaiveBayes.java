3158ec6df2901f2d63535a1a3482b50c746ae889
vitorcezli <vitor3365@gmail.com>
2017-05-27 18:54:09 -0300

    Update NaiveBayes.java

--- NaiveBayes.java
+++ NaiveBayes.java
@@ -16,17 +16,48 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * 
+ * Naive Bayes classification program
  * @author vitorcezar
  */
 public class NaiveBayes {
 
+    /**
+     * prints argument error and exit the program
+     */
     private static void printArgumentError() {
         System.out.println( "The arguments passed are incorrect" );
         System.exit( 1 );
     }
     
+    /**
+     * prints parser error and exit the program
+     * @param line the line where the error was found
+     */
+    private static void printLineError( int line ) {
+        System.out.printf( "Line %d is incorrect\n", line );
+        System.exit( 1 );
+    }
+    
+    /**
+     * prints file error and exit the program
+     */
+    private static void printFileError( String file ) {
+        System.out.printf( "File %s not found", file );
+        System.exit( 1 );
+    }
+    
+    /**
+     * verifies if the arguments passed are correct
+     * @param args the arguments
+     */
     private static void verifyArguments( String[] args ) {
         if( args.length == 0 ) {
             printArgumentError();
@@ -45,6 +76,93 @@ public class NaiveBayes {
     }
     
     /**
+     * train the Naive Bayes classifier with the training file
+     * @param classifier the Naive Bayes classifier
+     * @param path the training file path
+     */
+    private static void trainClassifier( NaiveBayesClassifier classifier, 
+        String path ) {
+        try {
+            Scanner trainingScanner = new Scanner( new File( path ) );
+            int line = 1;
+
+            while( trainingScanner.hasNext() ) {
+                
+                // variables to do a regex search
+                String lineRead = trainingScanner.nextLine();
+                Pattern numberPattern = Pattern.compile( "(\\d)," );
+                Pattern textPattern = Pattern.compile( "\"(.*)\"" );
+                Matcher numberMatcher = numberPattern.matcher( lineRead );
+                Matcher textMatcher = textPattern.matcher( lineRead );
+
+                // parses the file training the classifier
+                if( numberMatcher.find() && textMatcher.find() ) {
+                    
+                    // negative example
+                    if( numberMatcher.group( 1 ).equals( "0" ) ) {
+                        classifier.setNegativeExample( textMatcher.group( 1 ) );
+                    // positive example
+                    } else if( numberMatcher.group( 1 ).equals( "1" ) ) {
+                        classifier.setPositiveExample( textMatcher.group( 1 ) );
+                    // error
+                    } else {
+                        printLineError( line );
+                    }
+                // error
+                } else {
+                    printLineError( line );
+                }
+                line++;
+            }
+        } catch( FileNotFoundException e ) {
+            printFileError( path );
+        }
+    }
+    
+    /**
+     * classifies the texts of a file
+     * @param classifier the Naive Bayes classifier
+     * @param inputFilePath file that will be classified
+     * @param outputFile file where the classification will be printed
+     */
+    private static void classify( NaiveBayesClassifier classifier, 
+        String inputFilePath, String outputFile ) {
+        try {
+            Scanner trainingScanner = new Scanner( new File( inputFilePath ) );
+            PrintWriter writer = new PrintWriter( outputFile );
+            int line = 1;
+
+            while( trainingScanner.hasNext() ) {
+                // variables to do a regex search
+                String lineRead = trainingScanner.nextLine();
+                Pattern textPattern = Pattern.compile( "\"(.*)\"" );
+                Matcher textMatcher = textPattern.matcher( lineRead );
+
+                // parses the file classifying it
+                if( textMatcher.find() ) {
+                    int classification = 
+                        classifier.classify( textMatcher.group( 1 ) );
+                    if( classification == NaiveBayesClassifier.TRUE ) {
+                        double positiveProbability = 
+                            classifier.chanceOfPositive();
+                        writer.println( "" + 1 + "," + positiveProbability );
+                    } else {
+                        double negativeProbability =
+                            classifier.chanceOfNegative();
+                        writer.println( "" + 0 + "," + negativeProbability );
+                    }
+                } else {
+                    printLineError( line );
+                }
+                line++;
+            }
+            writer.close();
+        } catch( FileNotFoundException e ) {
+            printFileError( inputFilePath );
+        }
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main( String[] args ) {
@@ -52,22 +170,15 @@ public class NaiveBayes {
         
         if( args[ 0 ].equals( "-t" ) ) {
             NaiveBayesClassifier naiveBayes = new NaiveBayesClassifier();
-            naiveBayes.setPositiveExample( "I'm happy today" );
-            naiveBayes.setNegativeExample( "I'm sad today" );
+            trainClassifier( naiveBayes, args[ 1 ] );
             NaiveBayesClassifier.saveClassifier( args[ 2 ], naiveBayes );
         } else {
             try {
                 NaiveBayesClassifier naiveBayes = 
                     NaiveBayesClassifier.loadClassifier( args[ 1 ] );
-                if( naiveBayes.classify( "Happy I am" ) == NaiveBayesClassifier.TRUE ) {
-                    System.out.printf( "Positive example. Chance %f\n", 
-                        naiveBayes.chanceOfPositive() );
-                    System.out.printf( "Change of negative is %f\n", 
-                        naiveBayes.chanceOfNegative() );
-                }
+                classify( naiveBayes, args[ 2 ], args[ 3 ] );
             } catch( Exception e ) {
-                System.out.println( "The path is incorrect" );
-                System.exit( 1 );
+                printFileError( args[ 1 ] );
             }
         }
     }
